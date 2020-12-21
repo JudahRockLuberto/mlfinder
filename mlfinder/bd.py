5dc054a1b177836c831cf3aee0a678361fac9ee0
JudahRockLuberto <44139597+JudahRockLuberto@users.noreply.github.com>
2020-12-21 14:15:03 -0800

    Update bd.py

--- mlfinder/bd.py
+++ mlfinder/bd.py
@@ -50,6 +50,13 @@ class BrownDwarf():
         uncertainties = ['pm_ra', 'pm_dec', 'pm_pi', 'pm_mu_alpha', 'pm_mu_delta']
         are_uncertainties = [True if i in column_names else False for i in uncertainties]
         
+        print(are_uncertainties)
+        print(column_names)
+        print(uncertainties)
+        
+        print(all(are_uncertainties))
+        print(any(are_uncertainties))
+        
         if all(are_uncertainties) is False and any(are_uncertainties) is True:
             print('You only have some of the uncertainty values. Either include them all, or include none. All needed for ensuring good probabilities.')
             return None
