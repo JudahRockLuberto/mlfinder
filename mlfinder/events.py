57cddfe9901f7da8f5f9a8e73a0aee91e110b765
JudahRockLuberto <44139597+JudahRockLuberto@users.noreply.github.com>
2021-04-15 16:00:05 -0700

    Update events.py

--- mlfinder/events.py
+++ mlfinder/events.py
@@ -279,7 +279,7 @@ class FindEvents():
             delta_ml = self.delta_ml_calc(theta_min)
 
             close_df = self.add_to_close(close_df, self.bd.bd.object_name, theta_min, delta_ml, bd_ra, bd_dec, decals_id, bs_ra, bs_dec, mag, gaia_pointsource, time_of_min)
-        print('yo!', close_df)
+
         # now to find smallest sep in df or if lower than theta_max
         # find indices real quick
         if len(close_df) != 0:
