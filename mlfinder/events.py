6b77f75195ea72877a667bcb288d99586cf15f79
JudahRockLuberto <44139597+JudahRockLuberto@users.noreply.github.com>
2021-08-06 11:36:47 -0700

    Update events.py

--- mlfinder/events.py
+++ mlfinder/events.py
@@ -528,7 +528,7 @@ class FindEvents():
             # interpolate the shift
             interp_shift, interp_time = self.interpolate_shift(self.shift_df[name], self.shift_df['time'])
             
-            shift = ax1.scatter(interp_time, interp_shift, c=colors[i], s=2, label = number + r' M$_\mathrm{jup}$')
+            shift = ax1.scatter(interp_time, interp_shift, c=colors[i], s=4, label = number + r' M$_\mathrm{jup}$')
             
         # find where to set xlim based on peak: find point closest to half_max, get its index,
         # find the difference of max time to this time
@@ -593,7 +593,7 @@ class FindEvents():
             # reduce by 1
             interp_shift_reduced = np.array(interp_shift) - 1
             
-            shift = ax1.scatter(interp_time, interp_shift_reduced, s=4, label = number + r' M$_\mathrm{jup}$')
+            shift = ax1.scatter(interp_time, interp_shift_reduced, s=2, label = number + r' M$_\mathrm{jup}$')
             
         # find where to set xlim based on peak: find point closest to half_max, get its index,
         # find the difference of max time to this time
