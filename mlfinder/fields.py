cc3a934fbe6c6f9482a50a529b96f17af2e96805
JudahRockLuberto <44139597+JudahRockLuberto@users.noreply.github.com>
2021-12-05 21:01:14 -0800

    adding gaia compatibility

--- mlfinder/fields.py
+++ mlfinder/fields.py
@@ -4,6 +4,9 @@ import pandas as pd
 
 import astropy
 from astropy.table import Table
+from astropy.coordinates import SkyCoord
+
+from astroquery.gaia import Gaia
 
 # imports from datalab (installation: https://datalab.noao.edu/docs/manual/UsingTheNOAODataLab/InstallDatalab/InstallDatalab.html)
 import dl
@@ -103,3 +106,113 @@ class Fields():
         df= df[df.dered_mag_g <= 23.95]
 
         return df
+
+    ##
+    # Name: get_gaia_data
+    #
+    # inputs: index of star
+    # outputs: pandas table of position and motion (and errors) of star in gaia
+    #
+    # purpose: if the star isn't in gaia, it is far enough away to not have motion,
+    #          but this will find if the background star is in gaia (and need to calc motion)
+    def find_star_path(self, index):
+        # get data about star
+        row = self.stars.iloc[index]
+        ls_id = row.ls_id
+
+        # get gaia id of star from decals
+        q = """SELECT
+                ra1, dec1, id1, ra2, dec2, id2, distance
+            FROM
+                ls_dr9.x1p5__tractor__gaia_edr3__gaia_source
+            WHERE 
+                id1 = {} """.format(ls_id)
+
+        res = qc.query(sql=q)
+        decals_data = convert(res,'pandas')
+
+        # use gaia id to get info about it
+        query = """SELECT 
+                    source_id, ra, ra_error, dec, dec_error, parallax, pmra, pmra_error, pmdec, pmdec_error
+                FROM 
+                    gaiadr2.gaia_source
+                WHERE 
+                    source_id = {} """.format(decals_data.id2)
+
+        gaia_data = Gaia.launch_job(query).get_results().to_pandas()
+
+        return gaia_data
+    
+    ##
+    # Name: find_star_path
+    #
+    # inputs: index of star, mu_a and mu_d in mas, start time, end time, time in between each data point,
+    # outputs: path of that star
+    #
+    # purpose: find the path of a particular star
+    def find_star_path(self, index, parallax, mu_a, mu_d, start, end, step='1month'):
+        # creating an empty pandas dataframe bc easiest to work with
+        coord_df = pd.DataFrame(columns=['time', 'ra', 'dec'])
+
+        #first need to pull general data on star and convert to arcseconds
+        a_0 = self.stars.ra[index] * 3600
+        d_0 = self.stars.dec[index] * 3600
+
+        mu_a = self.mu_a / 1000
+        mu_d = self.mu_d / 1000
+
+        # make inputted times into jd -- note that t_split is temp, so i reuse for the observed date and the start date
+
+        # initial time
+        if type(self.observ_date) == float:
+            t_0 = self.observ_date
+        else:
+            t_split = self.observ_date.split('-')
+            t_0 = float(t_split[0]) + (strptime(t_split[1],'%b').tm_mon / 12) + (float(t_split[2]) / 365) #when observations happened
+
+        # start time
+        if type(start) == float:
+            t_start = start
+        else:
+            t_split = start.split('-')
+            t_start = float(t_split[0]) + (strptime(t_split[1],'%b').tm_mon / 12) + (float(t_split[2]) / 365) #when observations happened
+
+        # grab ephemerides in vector form
+        obj = Horizons(id='399', id_type='majorbody',
+                       epochs={'start':self.observ_date, 'stop':end,
+                               'step':step})
+
+        vectors = obj.vectors()
+        vectors = vectors['targetname', 'datetime_jd', 'x', 'y', 'z']
+
+        #run through each ephemeride coordinate/time (time as months)
+        for coord in vectors:
+            #converting coord to year
+            t = Time(float(coord[1]), format='jd')
+            t.format = 'jyear'
+            t = t.value
+
+            #cue formula for ra and dec at a given time.
+            d_prime = d_0 + (mu_d * (t - t_0))
+
+            #converting d to rad
+            d_prime_r = float(d_prime / 206265)
+
+            a_prime = a_0 + (mu_a * (t - t_0) / (np.cos(d_prime_r)))
+
+            #convert a to rad
+            a_prime_r = float(a_prime / 206265)
+
+            # actual equations
+            a_t = a_prime + ((pi_trig * ((coord[2] * np.sin(a_prime_r)) - (coord[3] * np.cos(a_prime_r))) / np.cos(d_prime_r)))
+            d_t = d_prime + (pi_trig * ((coord[2] * np.cos(a_prime_r) * np.sin(d_prime_r)) + (coord[3] * np.sin(a_prime_r) * np.sin(d_prime_r)) - (coord[4] * np.cos(d_prime_r))))
+
+            #convert a_t and d_t to degrees
+            a_t = a_t / 3600
+            d_t = d_t / 3600
+
+            #add to the coord dataframe,  but only if during or after when we want the start
+            if t > t_start:
+                coord_df = coord_df.append({'time': t, 'ra': a_t, 'dec': d_t}, ignore_index=True)
+
+        return coord_df
