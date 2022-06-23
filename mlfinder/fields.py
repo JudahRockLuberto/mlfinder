d377005dab961b90f8ea95899a3ee2c18b30dd9d
JudahRockLuberto <44139597+JudahRockLuberto@users.noreply.github.com>
2022-06-23 08:47:00 -0700

    Update fields.py

--- mlfinder/fields.py
+++ mlfinder/fields.py
@@ -148,7 +148,7 @@ class Fields():
                 FROM 
                     gaiadr2.gaia_source
                 WHERE 
-                    source_id = {} """.format(int(gaia_id))
+                    source_id = {} """.format(gaia_id.item())
         
         gaia_data = Gaia.launch_job(query).get_results().to_pandas()
 
